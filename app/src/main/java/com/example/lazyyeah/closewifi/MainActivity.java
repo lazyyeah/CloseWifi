2c971b20c2e8844eea84b43a73599ceddb823945
lazyyeahh <liziye123@gmail.com>
2015-11-03 12:59:57 +0800

    完善代码，独立按钮

--- app/src/main/java/com/example/lazyyeah/closewifi/MainActivity.java
+++ app/src/main/java/com/example/lazyyeah/closewifi/MainActivity.java
@@ -1,34 +1,32 @@
 package com.example.lazyyeah.closewifi;
 
-import android.app.PendingIntent;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.Intent;
-import android.database.ContentObserver;
 import android.location.LocationManager;
 import android.net.ConnectivityManager;
-import android.net.Uri;
 import android.net.wifi.WifiManager;
-import android.os.Handler;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 import java.lang.reflect.Method;
-import java.util.Observer;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
     private ConnectivityManager mCM;
     private WifiManager wifiManger;
     boolean gpsEnabled;
+    private Button buttonwifi;
     private Button buttonGps;
+    private Button buttongprs;
+    private Button buttonblueteeth;
     private Context context;
     private BluetoothAdapter mAdapter;
 
@@ -41,45 +39,68 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button) findViewById(R.id.button);
+
 
         wifiManger = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
-
-
         gpsEnabled = Settings.Secure.isLocationProviderEnabled(getContentResolver(), LocationManager.GPS_PROVIDER);
         Log.i("test", "GPS的状态是。。。" + gpsEnabled);
-        buttonGps = (Button) findViewById(R.id.button);
 
-        button.setOnClickListener(new View.OnClickListener() {
 
+        buttonwifi = (Button) findViewById(R.id.wifibutton);
+        buttonwifi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (wifiManger.isWifiEnabled()) {
-                    wifiManger.setWifiEnabled(false);
-                    System.out.println("wifi关上了");
-                } else {
-                    wifiManger.setWifiEnabled(true);
-                    System.out.println("wifi打开了");
-                }
-                gprsInit();
-
-/*启动gps界面
-                Intent intent = new Intent;
-                intent.setAction(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                context.startActivity(intent);
-*/
-                BluetoothInit();
-
-
+               wifiInit();
             }
-
         });
+
+
+        buttongprs = (Button) findViewById(R.id.gprsbutton);
+        buttongprs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gprsInit();
+            }
+        });
+
+
+        buttonblueteeth = (Button) findViewById(R.id.bluebutton);
+        buttonblueteeth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BluetoothInit();
+            }
+        });
+
+        buttonGps = (Button)findViewById(R.id.gpsbutton);
+        buttonGps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //启动gps界面
+                GPSInit();
+            }
+        });
+
+
+    }
+    private void wifiInit(){
+        if (wifiManger.isWifiEnabled()) {
+            wifiManger.setWifiEnabled(false);
+            System.out.println("wifi关上了");
+            Toast.makeText(getApplicationContext(), "wifi关上了",
+                    Toast.LENGTH_SHORT).show();
+
+        } else {
+            wifiManger.setWifiEnabled(true);
+            System.out.println("wifi打开了");
+            Toast.makeText(getApplicationContext(), "wifi打开了",
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void gprsInit() {
         mCM = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         gprsSetter();
-        finish();
     }
 
 
@@ -107,9 +128,13 @@ public class MainActivity extends AppCompatActivity {
         if (gprsisOpen) {
             setGprsEnabled("setMobileDataEnabled", false);
             System.out.println("GPRS关闭");
+            Toast.makeText(getApplicationContext(), "GPRS关闭",
+                    Toast.LENGTH_SHORT).show();
         } else {
             setGprsEnabled("setMobileDataEnabled", true);
             System.out.println("GPRS开启");
+            Toast.makeText(getApplicationContext(), "GPRS开启",
+                    Toast.LENGTH_SHORT).show();
         }
         return gprsisOpen;
     }
@@ -147,14 +172,25 @@ public class MainActivity extends AppCompatActivity {
     private void BluetoothInit() {
         mAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mAdapter.isEnabled() == false) {
-            System.out.println("蓝牙是关闭的");
             mAdapter.enable();
             System.out.println("蓝牙打开了");
+            Toast.makeText(getApplicationContext(), "蓝牙打开了",
+                    Toast.LENGTH_SHORT).show();
+
         } else {
-            System.out.println("蓝牙是打开的");
             mAdapter.disable();
             System.out.println("蓝牙关闭了");
+            Toast.makeText(getApplicationContext(), "蓝牙关闭了",
+                    Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void GPSInit(){
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(intent);
+        Toast.makeText(getApplicationContext(), "请用户手动关闭",
+                Toast.LENGTH_SHORT).show();
     }
 }
 
